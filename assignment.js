832da3ecc50f930ccd0edbc6dc1c94b2ac9686e6
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2024-05-28 17:28:50 -0700

    assignment js done

---
+++ assignment.js
@@ -0,0 +1,53 @@
+// YOU have to use a HOF.
+
+// 1. Write a function that takes a string parameter, and it console "YES" if the string,
+// contains 'y' other wise it console "NO".
+// Example - 'Crazyy' // YES
+// Example - Hello // NO
+
+function checkIfYExists(str) {
+    /// Write your logic here
+}
+
+checkIfYExists('crazy') // true
+
+// 2. Write a function that finds a factorial of a number.  5! ==> 5 * 4 * 3 * 2 * 1
+// Example - 5! = 120 (Use a normal loop for this one.) // ForEach
+
+function factorial(num) {
+    // 
+}
+
+// function getFactorial(num) {
+// logic
+// return the value;
+// }
+
+//3. You have an array of students
+// let studentList = [
+//   { name: "Mike", marks: [80, 50, 60, 100] },  290
+//   { name: "Daniel", marks: [40, 80, 100, 100] }, //
+//   {
+//     name: "Stacy",
+//     marks: [20, 100, 50, 70], 240
+//   },
+// ];
+
+// function getHighestAverageStudent() { // return string
+    // returns the student name with highest average marks
+// }
+
+//4. HARD Question - You have to write a function that has the highest number of occurrences
+// [ 20, 4, -10, 4, 11, 20, 4, 2]; // 4
+
+
+//5. You have to write a function that has to find a number which is unique in the array (I.e Only occured once)
+// [20, 20, 11, 4, 11, 20, 2, 4]
+
+
+// aba abba  abca  acca a 
+//6. You have an arryay of palindromes and not palindromes,  and you have to return only palindromes array
+
+// ['abc', 'aba', 'ccc', 'dca', 'a']
+
+// ['aba', 'ccc', 'a']
